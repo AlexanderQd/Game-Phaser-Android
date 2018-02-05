f8b10c5cfe16bba1387e07c0e3f655c5404e1519
AlexanderQd <manualex2193@gmail.com>
2018-02-05 18:18:51 +0000

    add dead and reset

--- www/js/states/Load.js
+++ www/js/states/Load.js
@@ -1,19 +1,20 @@
 let LoadState = {
     preload: () => {
         game.add.text(100, 100, 'loading...', {font: '30px Courier', fill: '#FFFFFF'})
-        game.load.image('player', 'assets/car.png');        
+        game.load.image('player', 'assets/player/car.png');        
         game.load.image('btnB','assets/GCW-Zero/GCW-Zero_B.png');
-        game.load.image('btnUp', 'assets/up.png');  
-        game.load.image('btnDown', 'assets/down.png');  
-        game.load.image('btnLeft', 'assets/left.png');  
-        game.load.image('btnRight', 'assets/right.png');  
-        game.load.image('center', 'assets/center.png');  
+        game.load.image('btnUp', 'assets/buttons/up.png');  
+        game.load.image('btnDown', 'assets/buttons/down.png');  
+        game.load.image('btnLeft', 'assets/buttons/left.png');  
+        game.load.image('btnRight', 'assets/buttons/right.png');  
+        game.load.image('center', 'assets/buttons/center.png');  
         game.load.image('btnX','assets/GCW-Zero/GCW-Zero_X.png');  
         game.load.audio('menuSound', 'assets/music/Menu.ogg');
         game.load.audio('levelSound', 'assets/music/gameLevel.ogg');
         game.load.tilemap('circuit', 'assets/maps/map.csv', null, Phaser.Tilemap.CSV);
         game.load.image('mappatterns', 'assets/maps/circuit.png');
-        
+        game.load.atlas('explosion', 'assets/player/explosion.png', 'assets/player/explosion.json');
+        game.load.image('dead','assets/maps/dead.png');
     },
 
     create: () => {
--- www/js/states/Play.js
+++ www/js/states/Play.js
@@ -2,34 +2,49 @@ let right = false;
 let left = false;
 let b = false;
 let x = false;
-let currentSpeed = 0;
+let currentSpeed;
+let timer, timeEvent;
 
 let playState = {
     
     create: () =>{
+        currentSpeed = 0;
 
         game.world.setBounds(0, 0, 3600, 3600);
+
         let map = game.add.tilemap('circuit', 12, 12);
         map.addTilesetImage('mappatterns');
+
         let layer = map.createLayer(0);
         layer.resizeWorld();
+
+        timer = game.time.create();
+        timerEvent = timer.add(Phaser.Timer.SECOND * 3, playState.endTimer, this);        
+        this.textTime = game.add.text(window.innerWidth + 200, window.innerHeight, "", {font: '60px Courier', fill: '#FFFFFF'});
+
+        this.dead = game.add.sprite(1500, 300, 'dead');
+        game.physics.enable(this.dead, Phaser.Physics.ARCADE);
+        game.physics.arcade.enableBody(this.dead);
         
-        this.player = game.add.sprite(200, 200, 'player');
+        this.player = game.add.sprite(900, 300, 'player');
         this.player.scale.setTo(0.1);
         game.physics.enable(this.player, Phaser.Physics.ARCADE);
         this.player.body.collideWorldBounds = true;
-        this.player.body.maxVelocity.setTo(120, 120);
+        this.player.body.maxVelocity.setTo(200, 200);
         game.camera.follow(player);
 
-        this.txt = game.add.text(100, 200, "Speed: ", {font: '15px Courier', fill: '#FFFFFF'});
-        this.txt.fixedToCamera = true;
 
-        let center = game.add.sprite(50,  window.innerHeight - 50, 'center');        
-        let up = game.add.sprite(center.position.x,center.position.y - 14, 'btnUp');
-        let down = game.add.sprite(center.position.x,center.position.y + 19, 'btnDown');
-        let buttonLeft = game.add.button(center.position.x - 14, center.position.y + 0.7, 'btnLeft', null);
-        let buttonRight = game.add.button(center.position.x + 19, center.position.y + 0.5, 'btnRight', null);
-        let buttonB = game.add.button(window.innerWidth - 150,  window.innerHeight - 50, 'btnB', null);
+
+        this.txt = game.add.text(20, 10, "Speed: ", {font: '15px Courier', fill: '#FFFFFF'});
+        this.txt.fixedToCamera = true;
+        
+
+        let center = game.add.sprite(50,  window.innerHeight - 70, 'center');        
+        let up = game.add.sprite(center.position.x,center.position.y -20, 'btnUp');
+        let down = game.add.sprite(center.position.x,center.position.y + 30, 'btnDown');
+        let buttonLeft = game.add.button(center.position.x - 22, center.position.y + 0.7, 'btnLeft', null);
+        let buttonRight = game.add.button(center.position.x + 30, center.position.y + 0.5, 'btnRight', null);
+        let buttonB = game.add.button(window.innerWidth - 150,  window.innerHeight - 60, 'btnB', null);
         let buttonX = game.add.button(window.innerWidth - 100,  window.innerHeight - 100, 'btnX', null);
 
        let array = [center, up, down, buttonLeft, buttonRight, buttonB, buttonX];
@@ -41,11 +56,11 @@ let playState = {
 
         buttonB.scale.setTo(0.5);
         buttonX.scale.setTo(0.5);
-        center.scale.setTo(0.5);
-        up.scale.setTo(0.5);
-        down.scale.setTo(0.5);
-        buttonLeft.scale.setTo(0.5);
-        buttonRight.scale.setTo(0.5);
+        center.scale.setTo(0.8);
+        up.scale.setTo(0.8);
+        down.scale.setTo(0.8);
+        buttonLeft.scale.setTo(0.8);
+        buttonRight.scale.setTo(0.8);
 
         buttonB.events.onInputDown.add(() => {b = true});
         buttonB.events.onInputUp.add(() => {b = false});
@@ -64,6 +79,8 @@ let playState = {
         music.onLoop.add(playState.playLevelMusic, music);
     },
     update: () => {
+    
+        game.physics.arcade.overlap(this.player, this.dead, playState.collisionHandlerDead, null, this);
 
         if(right){
             this.player.angle += 0.5;
@@ -72,7 +89,7 @@ let playState = {
         }
 
         if(x){
-            if(currentSpeed < 120)
+            if(currentSpeed < 200)
                 currentSpeed += 0.5;
         }else{
             if(b){
@@ -82,20 +99,45 @@ let playState = {
         }
 
         if (currentSpeed > 0)
-        {
-            game.physics.arcade.velocityFromRotation(this.player.rotation, currentSpeed, this.player.body.velocity);
+        {     
+           game.physics.arcade.velocityFromRotation(this.player.rotation, currentSpeed, this.player.body.velocity);
         }
         this.txt.setText(`Speed: ${Math.floor(this.player.body.speed)}`);
+
+        if (timer.running) {
+            this.textTime.setText(Math.round((timerEvent.delay - timer.ms) / 1000), 2, 14, "#ff0");
+        }else{
+            timer.stop();
+        }
         
     },
     render: () => {
-      
+        game.debug.text('currendSpeed '+ currentSpeed, 10, 40 );
         game.debug.text('player x '+ this.player.position.x, 10, 70 );
         
         game.debug.text('player y ' + this.player.position.y, 10, 90);
     },
     playLevelMusic: (music) => {        
         music.play('', 0, 1, true);
-    }
+    },
+    collisionHandlerDead: (player, dead) => {       
+        let expl = game.add.sprite(player.position.x, player.position.y, 'explosion');
 
+        this.player.kill();
+        
+        expl.scale.setTo(2);                
+        expl.animations.add('play', Phaser.Animation.generateFrameNames('bubble_explo',0, 10));
+        expl.animations.play('play', 5, false);
+        expl.animations.currentAnim.onComplete.add(() => {expl.kill()}, this);
+
+        playState.gameOver();
+
+    },
+    gameOver: () => {
+        timer.start();       
+        let prueba = game.add.text(window.innerWidth + 50, window.innerHeight - 100, "Game Over", {font: '60px Courier', fill: '#FFFFFF'});        
+        setTimeout(() => {
+            game.state.restart();
+        }, 3000);
+    }
 }
